68ec747e579b10227be7a5066edf4069c49abb6a
rakshitbharat <rakshitbharat@gmail.com>
2024-12-05 11:00:49 +0000

    Deploying to gh-pages from @ rakshitbharat/vscode-keyboard-sticker@4ad9a9be883b84e4f9333cf51cd06836b7e2354a 🚀

---
+++ _next/static/OKrIfV-8WYY5DbDcqH0L3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/675-b55f327799c39eab.js","static/chunks/pages/index-62a3e0d4cae96ce3.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
